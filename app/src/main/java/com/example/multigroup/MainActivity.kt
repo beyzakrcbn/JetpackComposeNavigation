5736206d490f855e24ae03ce05292dce80c5be69
beyzza <beyzkrc.0@gmail.com>
2025-07-31 03:30:07 +0300

    Ekranlar arası geçiş başarılı şekilde test edildi.

--- app/src/main/java/com/example/multigroup/MainActivity.kt
+++ app/src/main/java/com/example/multigroup/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.example.multigroup
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.multigroup.ui.theme.MultiGroupTheme
 import com.example.multigroup.ui.theme.navhost.SetUpNavHost
